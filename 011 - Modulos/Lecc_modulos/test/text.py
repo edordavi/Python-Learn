7df7209040d52e5f47582551531f10b8eb0cc5f3
Emil Daniel Ordoñez Avila <emil.ordonez@gmail.com>
2021-11-10 21:21:18 -0600

    Initial commit

---
+++ 011 - Modulos/Lecc_modulos/test/text.py
@@ -0,0 +1,8 @@
+import sys
+
+sys.path.insert(1, "C:\\Users\\emil_\\OneDrive\\learning\\2111 - Udemy Python desde Cero\\011 - Modulos\\Lecc_modulos")
+print(sys.path)
+
+from saludos import Saludo
+
+Saludo()
